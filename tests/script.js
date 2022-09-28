263c08a398d165df26f3dd47e10f05bb3552a677
rortega@spsolutions.com.mx <rortega-sps>
2022-09-28 12:15:29 -0500

    feat: test k6

---
+++ tests/script.js
@@ -0,0 +1,28 @@
+import { check, sleep } from 'k6';
+import http from 'k6/http';
+
+export const options = {
+  vus: 1,
+  duration: '30s',
+};
+
+export default function () {
+
+  const res = http.get('https://mule-test-preprod-xa2mx3.5sc6y6-1.usa-e2.cloudhub.io/test');
+
+  // Validaciones, cada validación genera un procentaje de error.
+  check(res, {
+    //Validación 1
+    'Hello test': (r) =>
+      r.body.includes('Hello test'),
+    // Validación 2
+    'Hello Mule': (r) =>
+      r.body.includes('Hello Mule'),
+  });
+
+  // Imprime la respuesta
+  console.log(res.body);
+
+  sleep(1);
+
+}
